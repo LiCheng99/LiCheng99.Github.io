469c23a0751f998de9778825e30d8796079cb3df
licheng <lyhc65@163.com>
2023-04-16 12:32:39 +0800

    Site updated: 2023-04-16 12:32:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/hello-world.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/hello-world.html","posts/licheng.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
